5b83298d3852377dceb3c194ac77546c4a201c17
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2022-11-09 10:06:42 +0530

    added file input button

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import filedialog
+import pandas as pd
+import numpy as np
+def openFile():
+    filepath = filedialog.askopenfilename(
+                                          title="Open file okay?",
+                                          filetypes= (("text files","*.csv"),
+                                          ("all files","*.*")))
+    file = open(filepath,'r')
+    df = pd.read_csv(filepath)
+    df.info()
+    print("-------------------------------------")
+    print(file.read())
+    file.close()
+
+window = Tk()
+window.title("CSV Visualizer")
+window.geometry('300x300')
+button = Button(text="Open CSV file",command=openFile)
+button.pack(pady=20)
+window.mainloop()
