9356b8a85fc309241133aa9a07a9ffacb9a1f7de
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2022-11-10 23:08:20 +0530

    provided a path for analyse.py to analyse

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from tkinter import *
 from tkinter import filedialog
 import pandas as pd
+from PIL import Image, ImageTk
+
 import numpy as np
+import sys
+import os 
+
 def openFile():
     filepath = filedialog.askopenfilename(title="Select the CSV file",
                                           filetypes= (("text files","*.csv"),
@@ -11,11 +16,16 @@ def openFile():
     df.info()
     print("-------------------------------------")
     print(file.read())
+    print("---------lets shift to another file------------")
+    os.system('python analyse.py')
     file.close()
 
+
 window = Tk()
+k=PhotoImage(file='./assets/images/open_csv_button.png')
+window.config(bg='#000000')
 window.title("CSV Visualizer")
 window.geometry('300x300')
-button = Button(text="Open CSV file",command=openFile)
+button = Button(window,command=openFile, image =k,bd=0,bg='#000000',activebackground="#000000")
 button.pack(pady=20)
 window.mainloop()
